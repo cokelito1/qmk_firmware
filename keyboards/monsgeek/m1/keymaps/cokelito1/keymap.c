9c2ce639a796a54d7ccd51ad182093c60316d567
cokelito1 <cokelitocokelito@gmail.com>
2023-12-22 03:17:15 -0300

    Added gaming layer plus enabled leader key on tab

--- keyboards/monsgeek/m1/keymaps/cokelito1/keymap.c
+++ keyboards/monsgeek/m1/keymaps/cokelito1/keymap.c
@@ -33,7 +33,8 @@ GAME_GG_EZ = SAFE_RANGE
 enum __layers {
     MAIN_LAYER,
     LATEX_LAYER,
-    CONFIG_LAYER
+    CONFIG_LAYER,
+    GAMING_LAYER
 };
 
 #define KC_TASK LGUI(KC_TAB)
@@ -43,12 +44,12 @@ enum __layers {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [MAIN_LAYER] = LAYOUT_all( /* Base */
-        KC_ESC,          KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,          KC_F11,  KC_F12,   KC_DEL,           KC_MEDIA_PLAY_PAUSE,
-        KC_GRV,          KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,            KC_MINS, KC_EQL,   KC_BSPC,          KC_HOME,
-        KC_TAB,          KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,            KC_LBRC, KC_RBRC,  KC_BSLS,          KC_PGUP,
-        MO(LATEX_LAYER), KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN,         KC_QUOT, KC_NUHS,  KC_ENT,           KC_PGDN,
-        KC_LSFT,         KC_NUBS, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,          KC_SLSH,           KC_RSFT, KC_UP,   TG(CONFIG_LAYER),
-        KC_LCTL,         KC_LGUI, KC_LALT,                   KC_SPC,                             KC_RALT, MO(LATEX_LAYER), KC_RCTL,           KC_LEFT, KC_DOWN, KC_RGHT),
+        KC_ESC,                   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,          KC_F11,  KC_F12,   KC_DEL,           KC_MEDIA_PLAY_PAUSE,
+        KC_GRV,                   KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,            KC_MINS, KC_EQL,   KC_BSPC,          KC_HOME,
+        LT(LATEX_LAYER, KC_TAB),  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,            KC_LBRC, KC_RBRC,  KC_BSLS,          KC_PGUP,
+        QK_LEAD,                  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN,         KC_QUOT, KC_NUHS,  KC_ENT,           TG(GAMING_LAYER),
+        KC_LSFT,                  KC_NUBS, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,          KC_SLSH,           KC_RSFT, KC_UP,   TG(CONFIG_LAYER),
+        KC_LCTL,                  KC_LGUI, KC_LALT,                   KC_SPC,                             KC_RALT, MO(LATEX_LAYER), KC_RCTL,           KC_LEFT, KC_DOWN, KC_RGHT),
 
     [LATEX_LAYER] = LAYOUT_all( /* Base */
         _______,         _______,     _______,      _______, _______, _______,    _______,                      _______,             _______,             _______,              _______,              _______, _______, _______,          _______,
@@ -65,6 +66,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______,         _______,     RGB_MOD,      _______, AS_RPT,  AS_TOGG,    _______,    _______,    RGB_SAI,    _______,    _______,    _______, _______, _______,          _______,
         _______,         RGB_HUD,     RGB_RMOD,     RGB_HUI, _______, _______,    _______,    AS_DOWN,    RGB_SAD,    AS_UP,      _______,    _______, _______, _______,          _______,
         _______,         _______,     _______,      _______, _______, _______,    _______,    _______,    _______,    _______,    _______,    _______,          _______, _______, TG(CONFIG_LAYER),
+        _______,         _______,     _______,                        _______,                                        _______,    _______,    _______,          _______, _______, _______),
+
+    [GAMING_LAYER] = LAYOUT_all( /* Base */
+        _______,         _______,     _______,      _______, _______, _______,    _______,    _______,    _______,    _______,    _______,    _______, _______, _______,          _______,
+        _______,         _______,     _______,      _______, _______, _______,    _______,    _______,    _______,    _______,    _______,    _______, _______, _______,          _______,
+        KC_TAB,          _______,     _______,      _______, _______, _______,    _______,    _______,    _______,    _______,    _______,    _______, _______, _______,          _______,
+        _______,         _______,     _______,      _______, _______, _______,    _______,    _______,    _______,    _______,    _______,    _______, _______, _______,          TG(GAMING_LAYER),
+        _______,         _______,     _______,      _______, _______, _______,    _______,    _______,    _______,    _______,    _______,    _______,          _______, _______, _______,
         _______,         _______,     _______,                        _______,                                        _______,    _______,    _______,          _______, _______, _______)
 };
 
@@ -117,12 +126,25 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
         && process_game_keycodes(keycode, record);
 }
 
+void leader_start_user() {}
+
+void leader_end_user() {
+    if(leader_sequence_one_key(KC_C)) {
+        SEND_STRING(SS_LCTL("c"));
+    } else if(leader_sequence_one_key(KC_V)) {
+        SEND_STRING(SS_LCTL("v"));
+    } else if(leader_sequence_one_key(KC_S)) {
+        tap_code16(KC_F13);
+    }
+}
+
 
 // clang-format off
 #if defined(ENCODER_MAP_ENABLE)
 const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
     [MAIN_LAYER] = { ENCODER_CCW_CW(KC_MEDIA_NEXT_TRACK, KC_MEDIA_PREV_TRACK) },
     [LATEX_LAYER] = { ENCODER_CCW_CW(KC_MEDIA_NEXT_TRACK, KC_MEDIA_PREV_TRACK) },
-    [CONFIG_LAYER] = { ENCODER_CCW_CW(RGB_VAI, RGB_VAD) }
+    [CONFIG_LAYER] = { ENCODER_CCW_CW(RGB_VAI, RGB_VAD) },
+    [GAMING_LAYER] = { ENCODER_CCW_CW(KC_MEDIA_NEXT_TRACK, KC_MEDIA_PREV_TRACK) }
 };
 #endif
